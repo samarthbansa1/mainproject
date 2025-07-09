22ddf4e819746dbbcdab3ab33fad5cb81598e291
Samarth Bansal <samarthbansal_23me237@dtu.ac.in>
2025-07-09 17:48:28 +0530

    changed to public ip address

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://13.223.56.111:8000",
 });
 
 // Request interceptor: attach access token
