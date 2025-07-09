b9c62a25c0921bd0e8cfb8294f93c7c3c187ff50
Samarth Bansal <samarthbansal_23me237@dtu.ac.in>
2025-07-09 17:55:32 +0530

    final ip change

--- frontend/src/pages/UserSignin.jsx
+++ frontend/src/pages/UserSignin.jsx
@@ -18,7 +18,7 @@ const UserSignin = () => {
     try {
       //code
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/auth/login/",
+        "http://13.223.56.111:8000/api/auth/login/",
         formData
       );
       console.log(response);
